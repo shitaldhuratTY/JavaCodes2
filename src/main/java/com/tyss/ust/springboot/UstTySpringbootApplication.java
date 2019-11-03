ce0139f2172167d1321a3edeffeaeade7f0a1d63
shitaldhurat3 <shital.d@testyantra.in>
2019-11-03 11:17:13 +0530

    Spring boot code 2

---
+++ src/main/java/com/tyss/ust/springboot/UstTySpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.tyss.ust.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UstTySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UstTySpringbootApplication.class, args);
+	}
+
+}
